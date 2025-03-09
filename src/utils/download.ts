0363d1543b0c8cf1c5c47d7ae5ea12e1edfbd8b3
ItsMeSamey <sameychain5041@gmail.com>
2025-03-09 06:33:27 +0530

    updated downloading.ts as well

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import parse, { HTMLElement } from "node-html-parser";
+import JSZip from 'jszip';
 
 export interface SubtitleInfo {
   fileName?: string;
@@ -70,6 +71,65 @@ export type LanguageID =
   'zh-tw' // Chinese Traditional
 ;
 
+export const LanguageNameMap: Record<LanguageID, string> = {
+  'an': 'Aragonese',
+  'ar': 'Arabic',
+  'at': 'Asturian',
+  'bg': 'Bulgarian',
+  'br': 'Breton',
+  'ca': 'Catalan',
+  'cs': 'Czech',
+  'da': 'Danish',
+  'de': 'German',
+  'el': 'Greek',
+  'en': 'English',
+  'eo': 'Esperanto',
+  'es': 'Spanish',
+  'et': 'Estonian',
+  'eu': 'Basque',
+  'fa': 'Persian',
+  'fi': 'Finnish',
+  'fr': 'French',
+  'gl': 'Galician',
+  'he': 'Hebrew',
+  'hi': 'Hindi',
+  'hr': 'Croatian',
+  'hu': 'Hungarian',
+  'hy': 'Armenian',
+  'id': 'Indonesian',
+  'is': 'Icelandic',
+  'it': 'Italian',
+  'ja': 'Japanese',
+  'ka': 'Georgian',
+  'km': 'Khmer',
+  'ko': 'Korean',
+  'mk': 'Macedonian',
+  'ms': 'Malay',
+  'nl': 'Dutch',
+  'no': 'Norwegian',
+  'oc': 'Occitan',
+  'pt-br': 'Brazilian Portuguese',
+  'pl': 'Polish',
+  'pt': 'Portuguese',
+  'ro': 'Romanian',
+  'ru': 'Russian',
+  'si': 'Sinhala',
+  'sk': 'Slovak',
+  'sl': 'Slovenian',
+  'sq': 'Albanian',
+  'sr': 'Serbian',
+  'sv': 'Swedish',
+  'th': 'Thai',
+  'tl': 'Tagalog',
+  'tr': 'Turkish',
+  'tt': 'Tatar',
+  'uk': 'Ukrainian',
+  'uz': 'Uzbek',
+  'vi': 'Vietnamese',
+  'zh': 'Chinese',
+  'zh-tw': 'Traditional Chinese',
+}
+
 interface FetchResponse {
   body: string,
   bodyUsed: true,
@@ -103,24 +163,39 @@ export async function fetchHtml(input: RequestInfo | URL, init?: RequestInit, er
 }
 
 export class DownloadedFile {
-  constructor(public filename: string, public data: string) {}
+  constructor(public data: string, public filename?: string) {}
 
-  static async download(url: string): Promise<DownloadedFile> {
+  static async download(url: string, filename?: string): Promise<DownloadedFile> {
     const response = await fetch(url);
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-    let filename: string | undefined;
     const contentDisposition = response.headers.get('content-disposition');
-    if (contentDisposition) {
+    if (contentDisposition && !filename) {
       const filenameMatch = contentDisposition.match(/filename="([^"]+?)"/);
       if (filenameMatch && filenameMatch[1]) {
         filename = filenameMatch[1];
       }
     }
 
-    return new DownloadedFile(filename!, await response.text());
+    let data: string;
+    if (filename?.endsWith('.zip')) {
+      return new DownloadedFile(await this.unpackzip(await response.arrayBuffer()), filename);
+    } else {
+      data = await response.text();
+    }
+
+    return new DownloadedFile(data, filename!);
+  }
+
+  private static async unpackzip(data: ArrayBuffer): Promise<string> {
+    const zip = await JSZip.loadAsync(data);
+
+    for (const relativePath in zip.files) {
+      if (zip.files[relativePath].dir) continue; // Skip directories
+      if (relativePath.endsWith('.srt')) return await zip.files[relativePath].async('text');
+    }
+
+    throw new Error(`Could not find subtitle file in ZIP\n${zip.files}`);
   }
 }
-
-
 
