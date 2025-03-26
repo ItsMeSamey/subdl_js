664a295ee607144c9abfec1015d028e3aa09f8b5
ItsMeSamey <sameychain5041@gmail.com>
2025-03-26 08:45:01 +0530

    added test file

---
+++ src/download/_test.ts
@@ -0,0 +1,39 @@
+import type { MovieList, SubtitleOptions } from '../utils/download';
+import fetchMovieSubtitlesOrg from './moviesubtitles.org';
+import fetchMoviesubtitlesrtCom from './moviesubtitlesrt.com';
+import fetchOpenSubtitlesCom from './opensubtitles.com';
+import fetchPodnapisiNet from './podnapisi.net';
+import fetchSubdlCom from './subdl.com';
+import fetchYifySubtitlesCh from './yifysubtitles.ch';
+
+async function testGetters(getter: (query: string, options: SubtitleOptions) => Promise<MovieList[]>) {
+  console.log(getter);
+
+  const list = await getter('The Matrix', {language: 'en'});
+  console.log(list);
+
+  const subtitles = await list[0].toSubtitleLinks();
+  console.log(subtitles[0]);
+
+  const file = await subtitles[0].download();
+  console.log(file.filename + ':', file.subtitles[0].slice(0, 100));
+}
+
+async function test() {
+  const funcs = [
+    fetchOpenSubtitlesCom,
+    //fetchMovieSubtitlesOrg,
+    //fetchYifySubtitlesCh,
+    //fetchSubdlCom,
+    //fetchPodnapisiNet,
+    //fetchMoviesubtitlesrtCom,
+  ];
+
+  for (const func of funcs) {
+    await testGetters(func);
+    console.log('\n---\n');
+  }
+}
+
+test();
+
